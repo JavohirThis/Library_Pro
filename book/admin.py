e413412564da96415c67e1854a46b4466ee4ef66
JavohirThis <javohir.this@gmail.com>
2023-07-18 19:50:10 -0400

    modified:   book/admin.py

--- book/admin.py
+++ book/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import (BookModel, AuthorModel, BookCategoryModel)
+from .models import (BookModel, AuthorModel, BookCategoryModel, CustomUser)
 admin.site.register(BookModel)
 admin.site.register(AuthorModel)
 admin.site.register(BookCategoryModel)
+admin.site.register(CustomUser)
