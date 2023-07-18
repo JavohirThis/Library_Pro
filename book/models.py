7ab826428926fd0e0fa30800e1375d3cbe72efd9
HusanboyKH <137888891+HusanboyKH@users.noreply.github.com>
2023-07-18 12:07:08 +0000

    modified:   book/models.py 	modified:   book/serializer.py 	modified:   book/urls.py 	modified:   book/views.py

--- book/models.py
+++ book/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('a','Admin'),
+        ('A','Author'),
+        ('b','Book')
+    )
+    roles = models.CharField(max_length=1,choices=ROLE_CHOICES)
+
+# Create your models here.
+
+class AuthorModel(models.Model):
+    name = models.CharField(max_length=50, default='')
+    fname = models.CharField(max_length=50, default='')
+    date_of_birth = models.DateField(default=datetime.now)
+    country=models.CharField(max_length=25,default='')
+    class Meta:
+        db_table = 'author'
+class BookCategoryModel(models.Model):
+    name = models.CharField(max_length=50, default='')
+class BookModel(models.Model):
+    category =models.ForeignKey(BookCategoryModel,on_delete=models.SET_NULL,null=True)
+    author = models.ForeignKey(AuthorModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, default='')
+    page = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField(default=1)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=None, null=True)
+    year=models.DateTimeField(default=datetime.now)
+    info=models.CharField(max_length=200,default='')
+    class Meta:
+        db_table = 'book'
--- book/views.py
+++ book/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< HEAD
+=======
+from django.shortcuts import render,get_object_or_404
+from rest_framework.views import APIView
+from .models import BookModel,AuthorModel
+from rest_framework.response import Response
+from rest_framework import generics
+from .serializers import BookSerializer,AuthorSerializer
+# Create your views here.
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+class AllBookView(generics.ListCreateAPIView):
+    queryset = BookModel.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class DetailDelBookView(generics.):
+    queryset = BookModel.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class UpdateBookView(generics.UpdateAPIView):
+    queryset = BookModel.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class DeleteBookView(generics.DestroyAPIView):
+    queryset = BookModel.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class AllAuthorView(generics.ListAPIView):
+    queryset = AuthorModel.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class DetailAuthorView(generics.RetrieveAPIView):
+    queryset = AuthorModel.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class CreateAuthorView(generics.ListCreateAPIView):
+    queryset = AuthorModel.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class UpdateAuthorView(generics.UpdateAPIView):
+    queryset = AuthorModel.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class DeleteAuthorView(generics.DestroyAPIView):
+    queryset = AuthorModel.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = (IsAuthenticated,)
+>>>>>>> 346c5e8 (first commit)
+
